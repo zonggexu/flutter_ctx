bb189a1acdd1d5afc5cc77795af226d82e254d35
Vanguard <397829762@qq.com>
2025-03-13 20:08:04 +0800

    add: 初始化

---
+++ android/app/src/main/kotlin/com/zongsen/flutter_ctx/flutter_ctx/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.zongsen.flutter_ctx.flutter_ctx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ctx/presentation/pages/root_page/root_page.dart';
+
+Future<void> loadOtherInit() async {}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await loadOtherInit();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.Protocols
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
